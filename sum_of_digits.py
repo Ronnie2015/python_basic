b11978d304ee7bb7d8076f132809efd2d886e233
Ronnie2015 <34644663+Ronnie2015@users.noreply.github.com>
2021-04-13 01:28:09 -0700

    Update sum_of_digits.py

--- sum_of_digits.py
+++ sum_of_digits.py
@@ -1,12 +1,25 @@
-#Sum of Digits
+#Sum of Digits iterative
 
 def SumOfDigits(n):
     Sum = 0
     
     while n > 0:
         Sum += n%10
-        n = n / 10
-    return int(Sum)
+        n = int(n/10)
+    return Sum
+
+n = 1234
+print(SumOfDigits(n))
+
+#recursive
+#Sum of Digits
+
+
+def SumOfDigits(n):
+    if n <= 0:
+        return 0
+    return n%10 + SumOfDigits(int(n/10))
+    
 
 n = 1234
 print(SumOfDigits(n))
